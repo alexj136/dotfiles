ce8a5d29121734d7618d5dd96c7a7607c8db7ad4
Alex Jeffery <apjeffery136@googlemail.com>
2014-07-15 23:15:47 +0100

    Fixed bug in links script where the link locations were wrong

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ Alex's Dotfiles Repository
 
 Uses symbolic links to track changes to various configuration files used in
 various places on the system. Run "sh links.sh" to place these links
-appropriately on a new system. View links.sh to see these locations.
+appropriately on a new system. View links.sh to see these locations. Use of the
+links script requires that this repo be pulled to '$HOME/.dotfiles/'.
--- links.sh
+++ links.sh
@@ -1,36 +1,37 @@
 #!/bin/sh
 
 # Creates required directories and symbolic links to the files in this
-# repository, in the required locations. Run with "sh links.sh".
+# repository, in the required locations. Run with 'sh links.sh'. This script
+# requires that the git repo exist at '$HOME/.dotfiles/'.
 
 # Command-line related things
-ln --symbolic --verbose gitconfig $HOME/.gitconfig
-ln --symbolic --verbose hgrc $HOME/.hgrc
-ln --symbolic --verbose zshrc $HOME/.zshrc
+ln -s --verbose $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+ln -s --verbose $HOME/.dotfiles/hgrc $HOME/.hgrc
+ln -s --verbose $HOME/.dotfiles/zshrc $HOME/.zshrc
 
 # X.Org related things
-ln --symbolic --verbose xinitrc $HOME/.xinitrc
-ln --symbolic --verbose Xresources $HOME/.Xresources
-ln --symbolic --verbose gtkrc-2.0 $HOME/.gtkrc-2.0
+ln -s --verbose $HOME/.dotfiles/xinitrc $HOME/.xinitrc
+ln -s --verbose $HOME/.dotfiles/Xresources $HOME/.Xresources
+ln -s --verbose $HOME/.dotfiles/gtkrc-2.0 $HOME/.gtkrc-2.0
 mkdir --parents --verbose $HOME/.config/fontconfig/
-ln --symbolic --verbose fonts.conf $HOME/.config/fontconfig/fonts.conf
+ln -s --verbose $HOME/.dotfiles/fonts.conf $HOME/.config/fontconfig/fonts.conf
 
 # i3WM config file
 mkdir --parents --verbose $HOME/.i3/
-ln --symbolic --verbose i3config $HOME/.i3/config
+ln -s --verbose $HOME/.dotfiles/i3config $HOME/.i3/config
 
 # XMonad related things
 mkdir --parents --verbose $HOME/.xmonad/
-ln --symbolic --verbose xmonad.hs $HOME/.xmonad/xmonad.hs
-ln --symbolic --verbose conkyrc $HOME/.conkyrc
+ln -s --verbose $HOME/.dotfiles/xmonad.hs $HOME/.xmonad/xmonad.hs
+ln -s --verbose $HOME/.dotfiles/conkyrc $HOME/.conkyrc
 
 # Openbox related things
-ln --symbolic --verbose pypanelrc $HOME/.pypanelrc
+ln -s --verbose $HOME/.dotfiles/pypanelrc $HOME/.pypanelrc
 mkdir --parents --verbose $HOME/.config/openbox/
-ln --symbolic --verbose autostart $HOME/.config/openbox/autostart
-ln --symbolic --verbose menu.xml $HOME/.config/openbox/menu.xml
-ln --symbolic --verbose rc.xml $HOME/.config/openbox/rc.xml
+ln -s --verbose $HOME/.dotfiles/autostart $HOME/.config/openbox/autostart
+ln -s --verbose $HOME/.dotfiles/menu.xml $HOME/.config/openbox/menu.xml
+ln -s --verbose $HOME/.dotfiles/rc.xml $HOME/.config/openbox/rc.xml
 
 # Other
 mkdir --parents --verbose $HOME/.config/zathura/
-ln --symbolic --verbose zathurarc $HOME/.config/zathura/zathurarc
+ln -s --verbose $HOME/.dotfiles/zathurarc $HOME/.config/zathura/zathurarc
