3a4b0ba8c66d7a7b8612cc40386620d52d9b3752
Alex Jeffery <apjeffery136@googlemail.com>
2015-01-23 11:42:29 +0000

    Removed trailing whitespace from xmonad.hs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -23,7 +23,7 @@ import System.Directory (getHomeDirectory)
 import qualified XMonad.StackSet               as W
 import qualified XMonad.Actions.FlexibleResize as F
 import qualified Data.Map                      as M
- 
+
 ------------------------------------------------------------------------
 -- Simple settings
 
@@ -37,59 +37,59 @@ myLogBGColor         = "#002b36" -- "#111111"
 myModMask            = mod4Mask
 myWorkspaces         = map show [1..9]
 myFocusFollowsMouse  = True
- 
+
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
- 
+
     -- launch a terminal
     [ ((modm              , xK_Return), spawn $ XMonad.terminal conf)
     , ((modm              , xK_t     ), spawn $ XMonad.terminal conf)
- 
+
     -- launch firefox
     , ((modm,               xK_y     ), spawn "firefox" )
 
     -- launch zathura
     , ((modm,               xK_u     ), spawn "zathura" )
- 
+
     -- launch dmenu
     , ((modm,               xK_d     ), spawn "exe=`dmenu_run -b` && eval \"exec $exe\"")
- 
-    -- close focused window 
+
+    -- close focused window
     , ((modm .|. shiftMask, xK_q     ), kill)
- 
+
      -- Rotate through the available layout algorithms
     , ((modm,               xK_space ), sendMessage NextLayout)
- 
+
     --  Reset the layouts on the current workspace to default
     , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
- 
+
     -- Resize viewed windows to the correct size
     , ((modm,               xK_n     ), refresh)
- 
+
     -- Cycle non-empty workspaces with mod+tab or mod+shift+tab
     , ((modm,               xK_Tab   ), moveTo Next NonEmptyWS)
     , ((modm .|. shiftMask, xK_Tab   ), moveTo Prev NonEmptyWS)
- 
+
     -- Move focus to the next window
     , ((modm,               xK_j     ), windows W.focusDown)
- 
+
     -- Move focus to the previous window
     , ((modm,               xK_k     ), windows W.focusUp)
- 
+
     -- Move focus to the master window
     , ((modm,               xK_m     ), windows W.focusMaster)
- 
+
     -- Swap the focused window and the master window
     , ((modm .|. shiftMask, xK_Return), windows W.swapMaster)
- 
+
     -- Swap the focused window with the next window
     , ((modm .|. shiftMask, xK_j     ), windows W.swapDown)
- 
+
     -- Swap the focused window with the previous window
     , ((modm .|. shiftMask, xK_k     ), windows W.swapUp)
- 
+
     -- Shrink/Expand the master area
     , ((modm              , xK_h     ), sendMessage Shrink)
     , ((modm              , xK_l     ), sendMessage Expand)
@@ -100,16 +100,16 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- Push window back into tiling
     , ((modm              , xK_i     ), withFocused $ windows . W.sink)
- 
+
     -- Increment the number of windows in the master area
     , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
- 
+
     -- Deincrement the number of windows in the master area
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
- 
+
     -- Quit xmonad
     , ((modm .|. shiftMask, xK_x     ), io (exitWith ExitSuccess))
- 
+
     -- Restart xmonad
     , ((modm .|. shiftMask, xK_c     ), restart "xmonad" True)
 
@@ -126,21 +126,20 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask, xK_z     ), spawn "xscreensaver-command --lock")
     ]
     ++
- 
+
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
     [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
     ++
- 
+
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
- 
- 
+
 ------------------------------------------------------------------------
 -- Volume commands: commands that are issued when a key combination is
 -- activated to adjust volume
@@ -153,24 +152,24 @@ myAudioMuteCommand  = "amixer set Master toggle"
 -- Mouse bindings: default actions bound to mouse events
 
 myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
- 
+
     -- mod-button1, Set the window to floating mode and move by dragging
     [ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))
- 
+
     -- mod-button2, Raise the window to the top of the stack
     , ((modMask, button2), (\w -> focus w >> windows W.swapMaster))
- 
+
     -- mod-button3, Set the window to floating mode and resize by dragging
     , ((modMask, button3), (\w -> focus w >> F.mouseResizeWindow w))
- 
+
     -- Scroll through non-empty workspaces with mod+scrollwheel
     , ((modMask, button4), (\w -> moveTo Prev NonEmptyWS))
     , ((modMask, button5), (\w -> moveTo Next NonEmptyWS))
     ]
- 
+
 ------------------------------------------------------------------------
 -- Layouts
- 
+
 -- You can specify and transform your layouts by modifying these values.
 -- If you change layout bindings be sure to use 'mod-shift-space' after
 -- restarting (with 'mod-c') to reset your layout state to the new
@@ -190,10 +189,10 @@ myLayout =
     delta   = 3/100    -- Percent of screen to increment by when resizing panes
     ratio   = 1/2      -- Default proportion of screen occupied by master pane
     slaves  = [1]
- 
+
 ------------------------------------------------------------------------
 -- Window rules
- 
+
 -- Execute arbitrary actions and WindowSet manipulations when managing
 -- a new window. You can use this to, for example, always float a
 -- particular program, or have a client always appear on a particular
@@ -210,7 +209,7 @@ myManageHook = composeAll
     [ title =? "Firefox Preferences" --> doFloat
     , isFullscreen --> (doF W.focusDown <+> doFullFloat)
     , manageDocks ]
- 
+
 ------------------------------------------------------------------------
 -- Status bars and logging
 
@@ -230,10 +229,10 @@ myLogHook h = dynamicLogWithPP $ dzenPP
     pad str = ' ' : str ++ " "
 
     colorize :: String -> String
-    colorize = dzenColor myLogFG1Color myLogFG2Color 
+    colorize = dzenColor myLogFG1Color myLogFG2Color
 
     colorizeFlipped :: String -> String
-    colorizeFlipped = dzenColor myLogFG2Color myLogFG1Color 
+    colorizeFlipped = dzenColor myLogFG2Color myLogFG1Color
 
     cleanLayout :: String -> String
     cleanLayout s = case s of
@@ -247,14 +246,14 @@ myLogHook h = dynamicLogWithPP $ dzenPP
 
 ------------------------------------------------------------------------
 -- Startup hook
- 
+
 -- Perform an arbitrary action each time xmonad starts or is restarted. Used,
 -- for example, by XMonad.Layout.PerWorkspace to initialize per-workspace layout
 -- choices.
 
 -- Set the window manager name to LG3D to make Java Swing apps cooperate
 myStartupHook = setWMName "LG3D"
- 
+
 ------------------------------------------------------------------------
 -- Now run xmonad with the appropriate defaults
 
