ed078aeb11725019c60b4cef4f2ce759226b0a41
Alex Jeffery <apjeffery136@googlemail.com>
2017-12-07 16:53:16 +0000

    removed luakit stuff

--- luakit_get_adblock_list_if_missing.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Get the Luakit adblock list if it doesn't already exist
-mkdir -p $HOME/.local/share/luakit
-wget --output-document=$HOME/.local/share/luakit/easylist.txt \
-    https://easylist-downloads.adblockplus.org/easylist.txt
