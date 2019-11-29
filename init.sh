b400fcfdd8be87c61f68d34f8a74a2af3f44ead8
Alex Jeffery <alex@compilerworks.com>
2019-11-29 16:15:13 +0000

    init script now sets up neovim

--- init.sh
+++ init.sh
@@ -3,5 +3,11 @@
 ln -s ~/.vim/vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle/
 mkdir -p ~/.vim/swapfiles/
+mkdir -p ~/.config/nvim/
+NVIM=~/.config/nvim/init.vim
+touch $NVIM
+echo 'set runtimepath^=~/.vim runtimepath+=~/.vim/after' > $NVIM
+echo 'let &packpath = &runtimepath' >> $NVIM
+echo 'source ~/.vimrc' >> $NVIM
 git clone https://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
