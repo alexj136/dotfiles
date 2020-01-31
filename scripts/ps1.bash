f58169fdbd9ec3ff1920926d077a2fe09bbd4f8b
Alex Jeffery <alex@compilerworks.com>
2020-01-31 15:00:30 +0000

    actually git ps1 again, but no colors

--- scripts/ps1.bash
+++ scripts/ps1.bash
@@ -1,11 +1,24 @@
 blue="\033[0;34m"
 reset="\033[0m"
 
-PS1=""
+function git_bit {
+    local blue="\033[0;34m"
+    local reset="\033[0m"
+    status_str="$(git status 2> /dev/null)"
+    if [ $? -ne 0 ]; then return 0; fi
+    printf "$blue[$reset"
+    printf "$status_str" | grep "On branch" | awk '{print $3}' | tr -d '\n'
+    printf "$blue]$reset "
+    unset status_str
+}
+
+PS1=" "
 if [[ -n $SSH_CLIENT ]]; then
-    PS1+=" \[$blue\][\[$reset\]\u@\h\[$blue\]]\[$reset\]"
+    PS1+="\[$blue\][\[$reset\]\u@\h\[$blue\]]\[$reset\] "
 fi
-PS1+=" \[$blue\][\[$reset\]\W\[$blue\]]\[$reset\] \$ "
+PS1+="\[$blue\][\[$reset\]\W\[$blue\]]\[$reset\] "
+PS1+="\$(git_bit)"
+PS1+="\$ "
 
 unset blue
 unset reset
